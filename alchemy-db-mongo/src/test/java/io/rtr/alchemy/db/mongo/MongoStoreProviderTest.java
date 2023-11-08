d3d2705f2f6cf3d8993b45842d995740791b79dd
Matthew Read <MatthewRyanRead@users.noreply.github.com>
2023-11-08 16:07:33 -0500

    Build with Java 21 and use Testcontainers for MongoDB

--- alchemy-db-mongo/src/test/java/io/rtr/alchemy/db/mongo/MongoStoreProviderTest.java
+++ alchemy-db-mongo/src/test/java/io/rtr/alchemy/db/mongo/MongoStoreProviderTest.java
@@ -1,20 +1,35 @@
 package io.rtr.alchemy.db.mongo;
 
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.MongoClientURI;
+import com.mongodb.MongoCredential;
 import com.mongodb.MongoException;
 import io.rtr.alchemy.db.ExperimentsStoreProvider;
 import io.rtr.alchemy.testing.db.ExperimentsStoreProviderTest;
-import org.junit.Ignore;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.junit.Assert.fail;
 
-@Ignore("must be run manually and requires a local Mongo instance")
+@Testcontainers
 public class MongoStoreProviderTest extends ExperimentsStoreProviderTest {
     private static final String DATABASE_NAME = "test_experiments";
 
+    @Container static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.14");
+
     @Override
     protected ExperimentsStoreProvider createProvider() {
-        return MongoStoreProvider.newBuilder().setDatabase(DATABASE_NAME).build();
+        final String connStr = mongoDBContainer.getReplicaSetUrl(DATABASE_NAME);
+        final MongoClientURI uri = new MongoClientURI(connStr, MongoClientOptions.builder());
+        MongoCredential credential =
+                MongoCredential.createCredential(
+                        uri.getUsername(), DATABASE_NAME, uri.getPassword());
+        return MongoStoreProvider.newBuilder()
+                .addCredential(credential)
+                .setDatabase(DATABASE_NAME)
+                .build();
     }
 
     @Override
